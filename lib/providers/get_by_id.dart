cac2d51954a1c94b4747f7a3361248350b2cf9eb
Jorge Saieg <jorgesaieg@gmail.com>
2021-05-13 15:51:04 -0300

    0.4.1

--- lib/providers/get_by_id.dart
+++ lib/providers/get_by_id.dart
@@ -1,18 +1,43 @@
+import 'package:flutter/material.dart';
+
 import 'package:dio/dio.dart';
+
 import 'package:my_imdb/models/movie.dart';
+import 'package:my_imdb/providers/favorites_provider.dart';
+import 'package:provider/provider.dart';
 
-Future<List<Movie>> getById(int id) async {
-  try {
-    Response response = await Dio().get(
-        'https://api.themoviedb.org/3/movie/$id?api_key=0e685fd77fb3d76874a3ac26e0db8a4b&language=es');
+class IdsProvider extends ChangeNotifier {
+  List<Movie> _peliculas = [];
 
-    final json = response.data;
+  List<Movie> get peliculas => _peliculas;
 
-    var byIdMovie = json.map((byIdJson) => Movie.fromJson(byIdJson)).toList();
+  void setPeluculas(List<Movie> peliculas) {
+    _peliculas = peliculas;
+    notifyListeners();
+  }
 
-    return List<Movie>.from(byIdMovie);
-  } catch (e) {
-    print(e);
-    return [];
+  final favList = Provider.of<FavoritesProvider>(context);
+
+  IdsProvider() {
+    for (var item in favList.favorites) {
+      Movie movie = getById(item);
+      _peliculas.add(movie);
+    }
+  }
+
+  Future<Movie> getById(String id) async {
+    try {
+      Response response = await Dio().get(
+          'https://api.themoviedb.org/3/movie/$id?api_key=0e685fd77fb3d76874a3ac26e0db8a4b&language=es');
+
+      final json = response.data;
+
+      Movie byIdMovie = json.map((byIdJson) => Movie.fromJson(byIdJson));
+
+      return byIdMovie;
+    } catch (e) {
+      print(e);
+      return e;
+    }
   }
 }
