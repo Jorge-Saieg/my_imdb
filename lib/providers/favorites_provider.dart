17c81fad1ea7fa817e3a743e33039f95808a3c41
Guido <38866056+guidoperman1986@users.noreply.github.com>
2021-05-25 21:43:27 -0300

    modificacion fav_icon

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -24,17 +24,17 @@ class FavoritesProvider extends ChangeNotifier {
   // }
 
   setFavorite(Movie movie) {
-    if (_favorites.contains(movie)) {
-      _favorites.removeWhere((item) => item == jsonEncode(movie));
+    if (_favorites.contains(movie.id.toString())) {
+      _favorites.removeWhere((item) => item == movie.id.toString());
     } else {
-      _favorites.add(jsonEncode(movie));
+      _favorites.add(movie.id.toString());
     }
     notifyListeners();
   }
 
   Icon favIcon(Movie movie) {
     IconData icon;
-    if (_favorites.contains(jsonEncode(movie))) {
+    if (_favorites.contains(movie.id.toString())) {
       icon = Icons.favorite;
     } else {
       icon = Icons.favorite_border;
